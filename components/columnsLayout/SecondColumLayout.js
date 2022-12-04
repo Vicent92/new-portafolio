6512a1ecae6c6f6b862fdf43b37bf477b6d7229c
vicent92 <vicentgame92@gmail.com>
2022-12-04 16:50:49 -0300

    implementacion de las query para pantallas mayores de 800px

--- components/columnsLayout/SecondColumLayout.js
+++ components/columnsLayout/SecondColumLayout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Projects } from '../projects/Projects'
 import { Skills } from '../skils/Skils'
+import styled from './style.module.css'
 
 export const SecondColumLayout = () => {
   return (
-    <div>
+    <div className={styled.constainer_SecondColumLayout}>
         <Skills/>
 
         <Projects/>
